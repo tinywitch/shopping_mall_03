99f6760aaf2206fcc6326520e943a61391dc0255
Nguyen Trung Duc <ngtrduc@github.com>
2017-09-21 12:29:29 +0700

    active sale program

--- module/Admin/src/Controller/IndexController.php
+++ module/Admin/src/Controller/IndexController.php
@@ -22,9 +22,16 @@ class IndexController extends AbstractActionController
      */
 	private $entityManager;
 
-	public function __construct($entityManager)
+    /**
+     * Sale Program manager.
+     * @var Application\Service\SaleProgramManager
+     */
+    private $saleProgramManager;
+
+	public function __construct($entityManager, $saleProgramManager)
     {
-        $this->entityManager = $entityManager;        
+        $this->entityManager = $entityManager; 
+        $this->saleProgramManager = $saleProgramManager;
     }
 
     public function indexAction()
@@ -37,10 +44,15 @@ class IndexController extends AbstractActionController
     	$number_of_products = count($products);
     	$number_of_stores = count($stores);
 
+        $sale_prodgram_need_active = $this->saleProgramManager->getSaleProgramNeedActive();
+        $sale_prodgram_need_done = $this->saleProgramManager->getSaleProgramNeedDone();
+
         return new ViewModel([
             'number_of_users' => $number_of_users,
             'number_of_products' => $number_of_products,
-            'number_of_stores' => $number_of_stores
+            'number_of_stores' => $number_of_stores,
+            'sale_prodgram_need_active' => $sale_prodgram_need_active,
+            'sale_prodgram_need_done' => $sale_prodgram_need_done
             ]);
     }
 }
